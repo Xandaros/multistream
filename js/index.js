46b04346f716336ea030cbe43afb58f9b74fde39
Eva <hax@riseup.net>
2017-07-02 07:09:17 +0200

    Changed default layout

--- js/index.js
+++ js/index.js
@@ -1,34 +1,54 @@
 var config = {
 	content: [{
-		type: 'row',
+		type: 'column',
 		content: [
 			{
-				type:'component',
-				componentName: 'stream',
-				title: 'stream: summit1g (twitch)',
-				componentState: { channel: 'summit1g', service: 'twitch' }
+				type: 'row',
+				content:[{
+					type:'component',
+					componentName: 'stream',
+					title: 'stream: gW9f-pYmLYk (youtube)',
+					componentState: { channel: 'gW9f-pYmLYk', service: 'youtube' }
+				},
+				{
+					type:'component',
+					componentName: 'stream',
+					title: 'stream: summit1g (twitch)',
+					componentState: { channel: 'summit1g', service: 'twitch' }
+				},
+				{
+					type:'component',
+					componentName: 'chat',
+					title: 'chat: monstercat (mixer)',
+					componentState: { channel: 'monstercat', service: 'mixer' }
+				}]
 			},
 			{
-				type:'component',
-				componentName: 'chat',
-				title: 'chat: summit1g (twitch)',
-				componentState: { channel: 'summit1g', service: 'twitch' }
-			},
-			{
-				type:'component',
-				componentName: 'stream',
-				title: 'stream: monstercat (mixer)',
-				componentState: { channel: 'monstercat', service: 'mixer' }
-			},
-			{
-				type:'component',
-				componentName: 'chat',
-				title: 'chat: monstercat (mixer)',
-				componentState: { channel: 'monstercat', service: 'mixer' }
+				type: 'row',
+				content:[
+				{
+					type:'component',
+					componentName: 'chat',
+					title: 'chat: gW9f-pYmLYk (youtube)',
+					componentState: { channel: 'gW9f-pYmLYk', service: 'youtube' }
+				},
+				{
+					type:'component',
+					componentName: 'chat',
+					title: 'chat: summit1g (twitch)',
+					componentState: { channel: 'summit1g', service: 'twitch' }
+				},
+				{
+					type:'component',
+					componentName: 'stream',
+					title: 'stream: monstercat (mixer)',
+					componentState: { channel: 'monstercat', service: 'mixer' }
+				}]
 			}
 		]
 	}]
 };
+
 if(localStorage.multistream_layout) {
 	config = JSON.parse(localStorage.multistream_layout);
 }
@@ -37,32 +57,32 @@ var streamLayout = new GoldenLayout( config );
 
 streamLayout.registerComponent( 'stream', function( container, state ){
   switch(state.service) {
-    case 'youtube':
-      container.getElement().append($('<iframe allowfullscreen src="//gaming.youtube.com/embed/'+state.channel+'?autoplay=1" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    case 'twitch':
-      container.getElement().append($('<iframe allowfullscreen src="//player.twitch.tv/?channel='+state.channel.toLowerCase()+'&html5" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    case 'mixer':
-      container.getElement().append($('<iframe allowfullscreen src="//mixer.com/embed/player/'+state.channel.toLowerCase()+'" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    default:
-      console.log('Invalid service');
+	case 'youtube':
+	  container.getElement().append($('<iframe allowfullscreen src="//gaming.youtube.com/embed/'+state.channel+'?autoplay=1" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	case 'twitch':
+	  container.getElement().append($('<iframe allowfullscreen src="//player.twitch.tv/?channel='+state.channel.toLowerCase()+'&html5" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	case 'mixer':
+	  container.getElement().append($('<iframe allowfullscreen src="//mixer.com/embed/player/'+state.channel.toLowerCase()+'" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	default:
+	  console.log('Invalid service');
   }
 });
 streamLayout.registerComponent( 'chat', function( container, state ){
   switch(state.service) {
-    case 'youtube':
-      container.getElement().append($('<iframe allowfullscreen src="//gaming.youtube.com/live_chat?v='+state.channel+'&embed_domain='+window.location.host+'" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    case 'twitch':
-      container.getElement().append($('<iframe src="//www.twitch.tv/'+state.channel.toLowerCase()+'/chat?darkpopout" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    case 'mixer':
-      container.getElement().append($('<iframe src="//mixer.com/embed/chat/'+state.channel.toLowerCase()+'" frameborder="0" scrolling="no"></iframe>'));
-      break;
-    default:
-      console.log('Invalid service');
+	case 'youtube':
+	  container.getElement().append($('<iframe allowfullscreen src="//gaming.youtube.com/live_chat?v='+state.channel+'&embed_domain='+window.location.host+'" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	case 'twitch':
+	  container.getElement().append($('<iframe src="//www.twitch.tv/'+state.channel.toLowerCase()+'/chat?darkpopout" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	case 'mixer':
+	  container.getElement().append($('<iframe src="//mixer.com/embed/chat/'+state.channel.toLowerCase()+'" frameborder="0" scrolling="no"></iframe>'));
+	  break;
+	default:
+	  console.log('Invalid service');
   }
 });
 
